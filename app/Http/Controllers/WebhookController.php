b80f83687ab2a6cc1ec47ec2f2a42da765de7b3b
Michael O'Connor <mike@skyparkingservices.co.uk>
2019-02-23 18:52:23 +0000

    wip

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class WebhookController extends Controller
 {
-    public function emailImport($data)
+    public function emailImport(Request $data)
     {
         return response(
             [
